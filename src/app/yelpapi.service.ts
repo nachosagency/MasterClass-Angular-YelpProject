89ee3718e019ae3b98cba82cbdd8f1d9b908be57
Gilbert NZEKA <nzeka@technachos.com>
2020-03-01 17:45:01 +0100

    Adding Yelp API

--- src/app/yelpapi.service.ts
+++ src/app/yelpapi.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YelpapiService {
+  url_base: string = 'https://api.yelp.com/v3/businesses/search?';
+  bearer_token: string = '';
 
   constructor(private http: HttpClient) { }
 
   getBusinesses(search_term, address): Observable<any>{
-    return this.http.get('http://localhost:4200/assets/chinese+food+bordeaux.json');
+    let _url = this.url_base+'term='+search_term+'&location='+address+'';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+this.bearer_token
+    });
+
+    return this.http.get(_url, { headers: headers });
   }
 }
