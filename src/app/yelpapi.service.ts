a944b8b1ea223223c0535b916ed64d7fb4b63887
Gilbert NZEKA <nzeka@technachos.com>
2020-03-01 17:53:01 +0100

    Reverting to local JSON

--- src/app/yelpapi.service.ts
+++ src/app/yelpapi.service.ts
@@ -7,16 +7,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class YelpapiService {
-  url_base: string = 'https://api.yelp.com/v3/businesses/search?';
-  bearer_token: string = '';
+  //url_base: string = 'https://api.yelp.com/v3/businesses/search?';
+  //bearer_token: string = 'uPFVvL6aObdtBJUs1uWBTaRgaDaOpOhuWQVcFj3zIo30oqMXPqOCv5XyIYsZkXkd2gWBuVRp8GVl0nVUAQ17RfoFEZkBHBuhT3BhJgMfOwxZM4owN8sUF_93rYJBXXYx';
 
   constructor(private http: HttpClient) { }
 
   getBusinesses(search_term, address): Observable<any>{
-    let _url = this.url_base+'term='+search_term+'&location='+address+'';
+    /*let _url = this.url_base+'term='+search_term+'&location='+address+'';
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer '+this.bearer_token
+    });*/
+    let _url = 'http://localhost:4200/assets/chinese+food+bordeaux.json';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
     });
 
     return this.http.get(_url, { headers: headers });
